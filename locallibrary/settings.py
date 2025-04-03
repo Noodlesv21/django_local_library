eef2e7c393cbb8d2ef9251cbcb0bf4cb3b6c62fe
Noodlesv21 <royji@dukes.jmu.edu>
2025-04-02 21:02:13 -0400

    keeps not deploying

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -88,12 +88,13 @@ WSGI_APPLICATION = "locallibrary.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR}/db.sqlite3'
     )
 }
-
 
 
 
